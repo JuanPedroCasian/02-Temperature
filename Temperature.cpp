f47677c17ff9d94c1c8c47dff0644444c531333e
Juan Casian <jp.capo.98@gmail.com>
2017-08-12 20:40:40 -0500

    Temperature app

---
+++ Temperature.cpp
@@ -0,0 +1,75 @@
+#include <iostream>           //includes input and output conmands
+
+using namespace std;   //makes it unnecesary to write std:: before commands
+
+int main () {           //main proces of the program
+
+ double Value1, Answer;         /* declaration of a double value, which is a
+                                  number with decimals. (Ex: 3.1416)
+                                  */
+
+cout << "Write a temperature in Farenheit" <<endl;          /* Printin the
+                                        * the text between "" and jumping to
+                                        the next line with the incertion
+                                        operators.
+
+                                        Note: ";" is necesary to end a comand
+                                        */
+cout << "that you want to convert to celcious:" <<endl;
+
+cin >> Value1;        //setting the input value to variable "Value1"
+
+Answer = 5 * (Value1 - 32)/9;       /*Makes the conversion from Farenheit to
+                                      Celsius and then declares is as the value
+                                      of variable "Answer"
+                                      */
+
+cout << "A temperature of "; cout << Value1; cout << " degrees Fahrenheit is ";
+cout << Answer; cout << " in Celsus" <<endl; /* Using ; to separate each
+                                                cout comand, with the exception
+                                                of endl which needs no ; prior
+                                                to it
+                                                */
+
+if (Answer <= 100) {     /* first time using if conditioning comand, it is
+                            used in this way:     "if (some condition) {
+
+                            instructions if condition is true
+
+                          } else {
+
+                          instructions if condition is not true
+
+                        }"
+
+                        Note: inside the parenthesis you should use a logical
+                        or relational operator, which are:
+
+                        RELATIONAL OPERATORS
+                        ==  equal
+                        !=  not equal
+                        < smaller than
+                        > greater than
+                        <= smaller or equal than
+                        >= bigger or equal than
+
+                        LOGICAL OPERATORS
+                        !       NOT   Ex: !(5==5)     es false
+                        &&      AND   Ex: (5==5) && (6 > 6)   False
+                        ||      OR    Ex: (5==5) || (6 > 6)   True
+                        */
+
+
+
+  cout << "Water does NOT boil at this temperature (under typical conditions)."
+  <<endl;           //action if condition is true
+
+} else {
+
+  cout << "Water does boil at this temperature (under typical conditions)."
+  <<endl;           //action if condition is false
+
+}
+
+
+}
